b38ebadca9113e5ac259e5708c8926221ff31ebc
Griffin <35904717+gjbsaiia@users.noreply.github.com>
2018-07-21 19:15:37 -0400

    caught some errors

--- sendIP.sh
+++ sendIP.sh
@@ -8,31 +8,29 @@
 # allows quick changes to all smtp request specifiers
 
 #which server
-echo "helo some.server.here" < ~/Desktop/car/email_IP/commands/handshake.txt
+echo "helo some.server.here" > ~/Desktop/car/email_IP/commands/handshake.txt
 #address mail is coming from
-echo "mail from: <SomeAddress@gmail.edu>" < ~/Desktop/car/email_IP/commands/from.txt
+echo "mail from: <SomeAddress@gmail.edu>" > ~/Desktop/car/email_IP/commands/from.txt
 #address mail is going to
-echo "rcpt to: <SomeEmail@gmail.com>" < ~/Desktop/car/email_IP/commands/to.txt
+echo "rcpt to: <SomeEmail@gmail.com>" > ~/Desktop/car/email_IP/commands/to.txt
 #email content specifier
-echo "data" <~/Desktop/car/email_IP/commands/contents.txt
+echo "data" > ~/Desktop/car/email_IP/commands/contents.txt
 
 #actual message is stored in currentIP.txt
 
 #end of message command
-echo "." < ~/Desktop/car/email_IP/commands/endMsg.txt
+echo "." > ~/Desktop/car/email_IP/commands/endMsg.txt
 #close connection command
-echo "quit" < ~/Desktop/car/email_IP/commands/quit.txt
+echo "quit" > ~/Desktop/car/email_IP/commands/quit.txt
 
 
 #writes 'ip addr' command to file
 echo $(ip addr) > currentIP.txt;
 #python script that edits and rewrites the file, so it's just IPs
-python processIP.py;
+/bin/python ~/Desktop/car/email_IP/processIP.py;
 echo "current address written"
-sleep 1;
 echo "sending mail.."
-sleep 1;
 #calls a shell script that prints all smtp commands into the window
 #pipes this with an smtp server request
-./smtpCommands.sh | telnet smtp.server.here 25
+~/Desktop/car/email_IP/smtpCommands.sh | telnet smtp.server.here 25
 echo "address sent."
